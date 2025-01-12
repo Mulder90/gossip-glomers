81450363e4d1fe9107741b37f0c2b0fa21a4e282
Lorenzo Cinque <lore.cinque@gmail.com>
2025-01-12 16:39:04 +0000

    Better structure

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -8,42 +8,63 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+const (
+	msgBroadcast = "broadcast"
+	msgRead      = "read"
+	msgTopology  = "topology"
+)
+
+const (
+	respBroadcastOk = "broadcast_ok"
+	respReadOk      = "read_ok"
+	respTopologyOk  = "topology_ok"
+)
+
 type BroadcastMessage struct {
 	Type    string `json:"type"`
 	Message int    `json:"message"`
 	MsgID   int    `json:"msg_id"`
 }
 
+type Server struct {
+	node     *maelstrom.Node
+	messages []int
+}
+
+func NewServer() *Server {
+	return &Server{
+		node:     maelstrom.NewNode(),
+		messages: make([]int, 0),
+	}
+}
+
+func (s *Server) handleBroadcast(msg maelstrom.Message) error {
+	var body BroadcastMessage
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	s.messages = append(s.messages, body.Message)
+
+	return s.node.Reply(msg, map[string]any{"type": respBroadcastOk})
+}
+
+func (s *Server) handleRead(msg maelstrom.Message) error {
+	return s.node.Reply(msg, map[string]any{"type": respReadOk, "messages": s.messages})
+}
+
+func (s *Server) handleTopology(msg maelstrom.Message) error {
+	return s.node.Reply(msg, map[string]any{"type": respTopologyOk})
+}
+
 func main() {
-	n := maelstrom.NewNode()
+	server := NewServer()
 
-	var received_messages []int
+	server.node.Handle(msgBroadcast, server.handleBroadcast)
+	server.node.Handle(msgRead, server.handleRead)
+	server.node.Handle(msgTopology, server.handleTopology)
 
-	n.Handle("init", func(msg maelstrom.Message) error {
-		received_messages = []int{}
-		return n.Reply(msg, map[string]any{"type": "init_ok"})
-	})
-
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body BroadcastMessage
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		received_messages = append(received_messages, body.Message)
-
-		return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
-	})
-
-	n.Handle("read", func(msg maelstrom.Message) error {
-		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": received_messages})
-	})
-
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		return n.Reply(msg, map[string]any{"type": "topology_ok"})
-	})
-
-	if err := n.Run(); err != nil {
+	if err := server.node.Run(); err != nil {
 		log.Printf("ERROR: %s", err)
 		os.Exit(1)
 	}
